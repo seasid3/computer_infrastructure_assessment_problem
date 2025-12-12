0ec830595aef6b6836d947b59b9b7fe22623b91b
Orla Woods <orla.woods@gmail.com>
2025-12-12 12:23:53 +0000

    File saving plot

--- faang.py
+++ faang.py
@@ -2,42 +2,31 @@
 # time. The code will run
 # Author: Orla Woods
 
-
-# Import modules
-
 # Import os for file handling
 import os
 
 # Import pandas
 import pandas as pd
 
-# Import matplotlib for plotting
-import matplotlib.pyplot as plt
-
 # Dates and times
 import datetime as dt
 
-# Import yFinance (Yahoo Finance)    
+# Import yFinance   
 import yfinance as yf
 
-# Get data for the five companies (multiple tickers at once), using 
-# df to persist the data frame
-# https://ranaroussi.github.io/yfinance/
-
+# Define function to get data
 def get_data():
     # Define the FAANG stocks
     faang_stocks = ['META', 'AAPL', 'AMZN', 'NFLX', 'GOOG']
-
     # Get hourly data for the last 5 days
     data = yf.download(tickers=faang_stocks, period='5d', interval='1h')
-    
     return data
 
-# Get the data
-df = get_data()
+# Current data and time
+now = dt.datetime.now()
 
-# Print the first few rows of the dataframe
-print(df.head())
+# Define filename
+filename = "data/" + dt.datetime.now().strftime("%Y%m%d-%H%M%S") + ".csv"
 
-
-
+# Save to csv
+df.to_csv(filename)
