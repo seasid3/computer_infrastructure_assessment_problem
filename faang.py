a6b0e19c60fee7b2175557e6b37f114850fed48a
Orla Woods <orla.woods@gmail.com>
2025-12-17 11:15:06 +0000

    updating faang.py

--- faang.py
+++ faang.py
@@ -35,3 +35,44 @@ filename = "data/" + dt.datetime.now().strftime("%Y%m%d-%H%M%S") + ".csv"
 
 # Save to csv
 df.to_csv(filename)
+
+def plot_data():
+
+    # Locate the latest datafile
+    data_files = sorted(os.listdir('data/'))
+    latest_file = "data/" + data_files[-1]
+   
+    # Load datafile (csv)
+    df = pd.read_csv(latest_file, header=[0,1], index_col=0, parse_dates=True)
+    
+    # Convert index to datetime
+    df.index = pd.to_datetime(df.index)
+    
+    # Define the columns to plot
+    close_df = df['Close']
+
+    # Create a plot
+    fig, ax = plt.subplots(figsize=(10, 6))
+    close_df.plot(ax=ax)
+    plt.xlabel('Date')
+    plt.ylabel('Price (USD)') 
+    ax.legend(title='Stocks')  
+    
+    # Set the plot title to current date
+    ax.set_title(dt.datetime.now().strftime("%Y-%m-%d"))
+    
+    # Ensure 'plots/' folder exists
+    plots_dir = Path("plots")
+    plots_dir.mkdir(exist_ok=True)
+    
+    # Current date and time.
+    now = dt.datetime.now()
+    filename = plots_dir / f"{now:%Y%m%d-%H%M%S}.png"
+
+    # Save figure.
+    fig.savefig(filename, dpi=300)
+    plt.close(fig)
+    
+    print(f"Saved plot to {filename.resolve()}")
+    
+plot_data()
