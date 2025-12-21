907b84185a115067658b7f6b780af7c0bf39c948
Orla Woods <orla.woods@gmail.com>
2025-12-21 12:55:08 +0000

    Tidying comments

--- faang.py
+++ faang.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env python3
 
-# This code will download hourly FAANG stock data from yFinance and will plot closing prices over
-# time. The code will run
-# Author: Orla Woods
+# This code will download hourly FAANG stock data from yFinance and will plot closing prices over time. 
+# Author: Orla Woods.
 
-# Import os for file handling
+# Import os for file handling.
 import os
 
-# Import pandas
+# Import pandas.
 import pandas as pd
 
-# Import matplotlib for plotting
+# Import matplotlib for plotting.
 import matplotlib.pyplot as plt
 
-# Dates and times
+# Dates and times.
 import datetime as dt
 
-# Import yFinance   
+# Import yFinance.
 import yfinance as yf
 
-# Define function to get data
+# Define function to get data.
 def get_data():
-    # Define the FAANG stocks
+    # Define the FAANG stocks.
     faang_stocks = ['META', 'AAPL', 'AMZN', 'NFLX', 'GOOG']
-    # Get hourly data for the last 5 days
+    # Get hourly data for the last 5 days.
     data = yf.download(tickers=faang_stocks, period='5d', interval='1h')
     return data
 
-# Get the data
+# Get the data.
 df = get_data()
 
-# Current data and time
+# Current data and time.
 now = dt.datetime.now()
 
-# Define filename
+# Define filename.
 filename = "data/" + dt.datetime.now().strftime("%Y%m%d-%H%M%S") + ".csv"
 
-# Save to csv
+# Save to csv.
 df.to_csv(filename)
 
 # Define the plot_data() function.
